0f2bedccdae74e69476f33771f9509e49f2686a2
mansour20-meet <mansour20@meet.mit.edu>
2018-07-31 09:57:51 -0400

    Stopping here

---
+++ greeting.py
@@ -0,0 +1,16 @@
+name=input("what is your name?")
+
+print("hello "+name)
+
+name_length=len("Mansour")
+
+print("Your name is "+str(name_length)+" letters long")
+
+my_name="mansour"
+
+print("The first letter of you name is "+str(my_name[0]).capitalize()+" and the last letter is "+str(my_name[6]).capitalize())
+
+      
+
+
+
