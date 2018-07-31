4487e369c4a2eb3a6b7d685b4dc01e1f4a508c98
mansour20-meet <mansour20@meet.mit.edu>
2018-07-31 07:29:36 -0400

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,19 @@
+Language="Italian"
+translation="Ciao Mondo"
+print("The translation of hello world in "+Language+" is "+translation)
+
+Language="Esperanto"
+translation="Saluton Mondo"
+print("The translation of hello world in "+Language+" is "+translation)
+
+Language="Xhosa"
+translation="Molo Lizwe"
+print("The translation of hello world in "+Language+" is "+translation)
+
+Language="Zulu"
+translation="Sawubona Mhlaba"
+print("The translation of hello world in "+Language+" is "+translation)
+
+Language="Scots Gaelic"
+translation="Halo a Shaoghail"
+print("The translation of hello world in "+Language+" is "+translation)
